5d10fe115df9f31c4b3367cee54f36803da78cba
badon1933 <badon1933@gmail.com>
2023-12-11 09:17:38 +0700

    Menambahkan UI shopping cart

--- js/script.js
+++ js/script.js
@@ -12,13 +12,23 @@ document.querySelector("#search-button").onclick = (e) => {
 };
 
 /* Ketika hamburger menu di klik */
-document.querySelector("#hamburger-menu").onclick = () => {
+document.querySelector("#hamburger-menu").onclick = (e) => {
   navbarNav.classList.toggle("active");
+  e.preventDefault();
+};
+
+/* Toggle class active untuk shopping cart */
+const shoppingCart = document.querySelector(".shopping-cart");
+
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
 };
 
 /* Klik di luar elemen */
 const hamburger = document.querySelector("#hamburger-menu");
 const searchButton = document.querySelector("#search-button");
+const shoppingCartButton = document.querySelector("#shopping-cart-button");
 
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
@@ -27,5 +37,12 @@ document.addEventListener("click", function (e) {
 
   if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
     searchForm.classList.remove("active");
+  }
+
+  if (
+    !shoppingCartButton.contains(e.target) &&
+    !shoppingCart.contains(e.target)
+  ) {
+    shoppingCart.classList.remove("active");
   }
 });
